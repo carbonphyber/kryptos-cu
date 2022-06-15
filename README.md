991ccea9e9cdecf86cf6c89baf92cb35427e3317
David Wortham <djwortham@gmail.com>
2022-06-15 02:22:47 -0700

    fix(*): add code to decrypt K2 as well, assuming known key

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
+# kryptos-cu
 Cuda C++ implementation of [Kryptos](https://en.wikipedia.org/wiki/Kryptos) decryptor.
 
+## Libraries
 Depends on [Nvidia Thrust library](https://github.com/NVIDIA/thrust/) for [Nvidia Cuda](https://developer.nvidia.com/cuda-zone).
 Both require Nvidia GPU device.
 
-Once Thrust has been installed, these example programs can be compiled
-directly with nvcc. See [Makefile](./Makefile) for build command with `nvcc`.
+Once Thrust has been installed, these example programs can be compiled directly with nvcc. See [Makefile](./Makefile) for build command with `nvcc`.
+
+## 
--- src/kryptos.cu
+++ src/kryptos.cu
@@ -10,7 +10,9 @@
 #define K1_CIPHERTEXT "EMUFPHZLRFAXYUSDJKZLDKRNSHGNFIVJYQTQUXQBQVYUVLLTREVJYQTMKYRDMFD"
 #define K1_PLAINTEXT "BETWEENSUBTLESHADINGANDTHEABSENCEOFLIGHTLIESTHENUANCEOFIQLUSION"
 #define K1_KEY "PALIMPSEST"
-#define K1_ALPHABET "KRYPTOSABCDEFGHIJLMNQUVWXZ"
+#define K2_CIPHERTEXT "VFPJUDEEHZWETZYVGWHKKQETGFQJNCEGGWHKKDQMCPFQZDQMMIAGPFXHQRLGTIMVMZJANQLVKQEDAGDVFRPJUNGEUNAQZGZLECGYUXUEENJTBJLBQCRTBJDFHRRYIZETKZEMVDUFKSJHKFWHKUWQLSZFTIHHDDDUVHDWKBFUFPWNTDFIYCUQZEREEVLDKFEZMOQQJLTTUGSYQPFEUNLAVIDXFLGGTEZFKZBSFDQVGOGIPUFXHHDRKFFHQNTGPUAECNUVPDJMQCLQUMUNEDFQELZZVRRGKFFVOEEXBDMVPNFQXEZLGREDNQFMPNZGLFLPMRJQYALMGNUVPDXVKPDQUMEBEDMHDAFMJGZNUPLGEWJLLAETG"
+#define K2_KEY "ABSCISSA"
+#define KRYPTOS_ALPHABET "KRYPTOSABCDEFGHIJLMNQUVWXZ"
 #define NUM_OF_LETTERS 26
 
 // quagmire_three_decrypt
@@ -29,7 +31,7 @@ struct quagmire_three_decrypt : public thrust::binary_function<T,T,U>
     int alphabet_offset = cipher_char_offset
       - key_offset
       + (cipher_char_offset >= key_offset ? 0 : NUM_OF_LETTERS);
-    return K1_ALPHABET[alphabet_offset];
+    return KRYPTOS_ALPHABET[alphabet_offset];
   }
 };
 
@@ -70,7 +72,7 @@ int main(int argc, char **argv) {
   std::cout << "K1_CIPHERTEXT: " << K1_CIPHERTEXT << std::endl;
   std::cout << "K1_PLAINTEXT: " << K1_PLAINTEXT << std::endl;
   std::cout << "K1_KEY: " << K1_KEY << std::endl;
-  std::cout << "K1_ALPHABET: " << K1_ALPHABET << std::endl;
+  std::cout << "KRYPTOS_ALPHABET: " << KRYPTOS_ALPHABET << std::endl;
 
   int k1_len = strlen(K1_CIPHERTEXT);
 
@@ -81,14 +83,14 @@ int main(int argc, char **argv) {
     h_k1_ciphertext[i] = (unsigned char)this_cipher_letter;
     int this_alphabet_index = 0;
     for (int j = 0; j < NUM_OF_LETTERS; j++) {
-      if (this_cipher_letter == K1_ALPHABET[j]) this_alphabet_index = j;
+      if (this_cipher_letter == KRYPTOS_ALPHABET[j]) this_alphabet_index = j;
     }
     h_k1[i] = this_alphabet_index;
   }
 
   // print out IoC value
   double k1_ciphertext_ioc = calc_index_of_coincidence(h_k1_ciphertext);
-  std::cout << "IoC: " << k1_ciphertext_ioc << std::endl << std::endl;
+  std::cout << "K1 Ciphertext IoC: " << k1_ciphertext_ioc << std::endl << std::endl;
 
   // print out K1 indexes
   std::cout << "k1: ";
@@ -102,7 +104,7 @@ int main(int argc, char **argv) {
     int this_key_letter = K1_KEY[i % k1_key_len];
     int this_alphabet_index = 0;
     for (int j = 0; j < NUM_OF_LETTERS; j++) {
-      if (this_key_letter == K1_ALPHABET[j]) this_alphabet_index = j;
+      if (this_key_letter == KRYPTOS_ALPHABET[j]) this_alphabet_index = j;
     }
     h_k1_key[i] = this_alphabet_index;
   }
@@ -115,12 +117,67 @@ int main(int argc, char **argv) {
   thrust::host_vector<unsigned char> h_k1_decrypted(k1_len);
   thrust::transform(thrust::host, h_k1.begin(), h_k1.end(), h_k1_key.begin(), h_k1_decrypted.begin(), quagmire_three_decrypt<unsigned int, unsigned char>());
   std::cout << "h_k1_decrypted: ";
-  thrust::copy_n(h_k1_decrypted.begin(), k1_len, std::ostream_iterator<unsigned char>(std::cout, " "));
+  thrust::copy_n(h_k1_decrypted.begin(), k1_len, std::ostream_iterator<unsigned char>(std::cout, ""));
   std::cout << std::endl << std::endl;
 
   // print out IoC value
   double k1_plaintext_ioc = calc_index_of_coincidence(h_k1_decrypted);
   std::cout << "Decrypted IoC: " << k1_plaintext_ioc << std::endl << std::endl;
+
+
+  //
+  std::cout << "K2_CIPHERTEXT: " << K2_CIPHERTEXT << std::endl;
+  std::cout << "K2_KEY: " << K2_KEY << std::endl;
+  std::cout << "KRYPTOS_ALPHABET: " << KRYPTOS_ALPHABET << std::endl;
+
+  int k2_len = strlen(K2_CIPHERTEXT);
+
+  thrust::host_vector<unsigned int> h_k2(k2_len);
+  thrust::host_vector<unsigned int> h_k2_ciphertext(k2_len);
+  for(int i = 0; i < k2_len; i++) {
+    int this_cipher_letter = K2_CIPHERTEXT[i];
+    h_k2_ciphertext[i] = (unsigned char)this_cipher_letter;
+    int this_alphabet_index = 0;
+    for (int j = 0; j < NUM_OF_LETTERS; j++) {
+      if (this_cipher_letter == KRYPTOS_ALPHABET[j]) this_alphabet_index = j;
+    }
+    h_k2[i] = this_alphabet_index;
+  }
+
+  // print out IoC value
+  double k2_ciphertext_ioc = calc_index_of_coincidence(h_k2_ciphertext);
+  std::cout << "K2 Ciphertext IoC: " << k2_ciphertext_ioc << std::endl << std::endl;
+
+  // print out K2 indexes
+  std::cout << "k2: ";
+  thrust::copy_n(h_k2.begin(), k2_len, std::ostream_iterator<unsigned int>(std::cout, " "));
+  std::cout << std::endl << std::endl;
+
+  // repeat the key for the length of the k2 string
+  thrust::host_vector<unsigned int> h_k2_key(k2_len);
+  int k2_key_len = strlen(K2_KEY);
+  for(int i = 0; i < k2_len; i++) {
+    int this_key_letter = K2_KEY[i % k2_key_len];
+    int this_alphabet_index = 0;
+    for (int j = 0; j < NUM_OF_LETTERS; j++) {
+      if (this_key_letter == KRYPTOS_ALPHABET[j]) this_alphabet_index = j;
+    }
+    h_k2_key[i] = this_alphabet_index;
+  }
+  std::cout << "key: ";
+  thrust::copy_n(h_k2_key.begin(), k2_len, std::ostream_iterator<unsigned int>(std::cout, " "));
+  std::cout << std::endl << std::endl;
+
+  // transform k2 ciphertext indexes and k2 key indexes into k2 plaintext indexes
+  thrust::host_vector<unsigned char> h_k2_decrypted(k2_len);
+  thrust::transform(thrust::host, h_k2.begin(), h_k2.end(), h_k2_key.begin(), h_k2_decrypted.begin(), quagmire_three_decrypt<unsigned int, unsigned char>());
+  std::cout << "h_k2_decrypted: ";
+  thrust::copy_n(h_k2_decrypted.begin(), k2_len, std::ostream_iterator<unsigned char>(std::cout, ""));
+  std::cout << std::endl << std::endl;
+
+  // print out IoC value
+  double k2_plaintext_ioc = calc_index_of_coincidence(h_k2_decrypted);
+  std::cout << "K2 Decrypted IoC: " << k2_plaintext_ioc << std::endl << std::endl;
 
   return EXIT_SUCCESS;
 }
